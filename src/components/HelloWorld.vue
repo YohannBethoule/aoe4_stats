509b7eade928930483504e7bc4fc804cc2313a8f
Yohann Bethoule <yoyoteva@gmail.com>
2021-12-15 20:53:59 +0100

    ui enhancements

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,106 +18,12 @@
     </div>
 
 
-    <div class="container mx-auto px-4 sm:px-8 ">
-      <div class="py-8">
-        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-          <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-            <table class="min-w-full leading-normal">
-              <thead>
-              <tr>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  User
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  Ranking
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  Elo
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  ID
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  Games
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  Wins
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  Losses
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  Winrate
-                </th>
-                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                  Country
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="[profile_id, player] in leaderboards" :key="player.profile_id" @click="selectPlayer(player.profile_id)"
-                :class="{ 'bg-blue-500' : selectedPlayer.profile_id === player.profile_id}">
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.name}}
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.rank}}
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.rating}}
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.profile_id}}
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.games}}
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.wins}}
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.losses}}
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{(player.wins/player.games*100).toFixed(2)}} %
-                  </p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">
-                    {{player.country}}
-                  </p>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="w-3/4" v-if="playerCharge">
-      <v-skeleton-loader
-          class=""
-          type="card"
-      ></v-skeleton-loader>
-    </div>
+  <LeaderBoard :leaderboards="this.leaderboards"
+    v-on:select-player="selectPlayer($event)"></LeaderBoard>
 
-   <PlayerCard v-if="this.playerEstCharge"
-         :player="this.selectedPlayer"/>
+   <PlayerCard v-if="this.selectedPlayer"
+         :selected-player="this.selectedPlayer"
+         v-on:close-player-card="unselectPLayer()"/>
   </div>
 </template>
 
@@ -125,11 +31,13 @@
 import axios from 'axios';
 import PlayerCard from "@/components/PlayerCard";
 import Vue from "vue";
+import LeaderBoard from "@/components/LeaderBoard";
 
 
 export default {
   name: 'HelloWorld',
   components: {
+    LeaderBoard,
     PlayerCard
   },
   data() {
@@ -138,8 +46,6 @@ export default {
       leaderboards: new Map(),
       selectedPlayer: 0,
       constantes: [],
-      playerEstCharge: false,
-      playerCharge: false,
     }
   },
   beforeMount(){
@@ -172,40 +78,11 @@ export default {
       }
     },
     selectPlayer(profile_id) {
-      this.playerEstCharge = false;
-      this.playerCharge = true;
       this.selectedPlayer = {...this.leaderboards.get(profile_id)}
-      let requests = [this.getMatchHistory(profile_id), this.getRatingHistory(profile_id)]
-      Promise.all(requests).then(() => {
-            this.playerEstCharge = true;
-            this.playerCharge = false;
-          })
-
+      this.$vuetify.goTo(0)
     },
-    getMatchHistory(profile_id) {
-      return axios
-          .get('https://aoeiv.net/api/player/matches?game=aoe4&count=1000&profile_id=' + profile_id)
-          .then(response => {
-            this.leaderboards.get(profile_id).matchHistory = response.data
-            this.refreshData();
-          });
-    },
-    getRatingHistory(profile_id){
-      let requests = []
-      for (let leaderboard_id of this.$leaderboards) {
-        requests.push(axios
-            .get('https://aoeiv.net/api/player/ratinghistory?game=aoe4&leaderboard_id=' + leaderboard_id + '&count=1000&profile_id=' + profile_id)
-            .then(response => {
-              console.log('rating history ' + leaderboard_id, response )
-              let player = this.leaderboards.get(profile_id);
-              if (!player.ratingHistory) {
-                player.ratingHistory = new Map()
-              }
-              player.ratingHistory.set(leaderboard_id, {...response.data});
-              this.refreshData();
-            })
-        )
-      }
+    unselectPLayer() {
+      this.selectedPlayer = null;
     },
     getStringConstantes(langage){
       axios
---
+++ src/components/LeaderBoard.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container mx-auto px-4 sm:px-8 ">
+    <div class="py-8">
+      <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+        <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+          <table class="min-w-full leading-normal">
+            <thead>
+            <tr>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                User
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                Ranking
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                Elo
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                ID
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                Games
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                Wins
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                Losses
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                Winrate
+              </th>
+              <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                Country
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="[profile_id, player] in leaderboards" :key="player.profile_id" @click="$emit('select-player', profile_id)"
+                class="hover:bg-blue-200">
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.name}}
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.rank}}
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.rating}}
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.profile_id}}
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.games}}
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.wins}}
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.losses}}
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{(player.wins/player.games*100).toFixed(2)}} %
+                </p>
+              </td>
+              <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                <p class="text-gray-900 whitespace-no-wrap">
+                  {{player.country}}
+                </p>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeaderBoard",
+  props: {
+    leaderboards: Map,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
