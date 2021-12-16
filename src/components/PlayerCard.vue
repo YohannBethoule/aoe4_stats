1a3750803033ff6c7c3c49324ad89b08e88d7862
Yohann Bethoule <yoyoteva@gmail.com>
2021-12-16 22:54:45 +0100

    scrollable player card

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -1,59 +1,74 @@
 <template>
-
-  <v-card
-      class="mb-5 p-10 absolute top-10"
-      elevation="15"
+  <v-dialog
+      transition="dialog-bottom-transition"
       width="90%"
-      loading="playerCharge"
-      rounded
+      v-model="selectedPlayer"
+      scrollable
   >
-    <div class="flex justify-end">
-      <v-hover v-slot="{ hover }">
-        <v-btn
-            icon
-            :color="hover ? 'red' : 'blue'"
-            @click="$emit('close-player-card')"
-        >
-          <v-icon>mdi-close-outline</v-icon>
-        </v-btn>
-      </v-hover>
+    <v-card
+        class="mb-5 p-10 absolute top-10"
+        elevation="15"
+        loading="playerCharge"
+        width="90%"
+        rounded
+    >
+        <v-card-title>
+          <div class="flex h-30 w-full">
+            <div>
+              {{player.name}}
+            </div>
 
-    </div>
+            <div class="ml-auto">
+              <v-hover v-slot="{ hover }">
+                <v-btn
+                    icon
+                    :color="hover ? 'red' : 'blue'"
+                    @click="$emit('close-player-card')"
+                >
+                  <v-icon>mdi-close-outline</v-icon>
+                </v-btn>
+              </v-hover>
+
+            </div>
+          </div>
+        </v-card-title>
+      <v-card-text>
+        <div class="flex flex-col">
+
+          <div class="flex items-between">
+
+            <GameMode v-if="player" :data="player" label="Global"/>
+            <GameMode v-for="mode in constantes.leaderboards" :key="mode.id" :data="player[mode.id]" :label="mode.label"/>
+          </div>
+          <div class="flex items-center my-10">
+            <button v-for="mode in constantes.leaderboards" :key="mode.id" @click="() => {gameMode = mode.id}" type="button"
+                    :class="{'bg-blue-500' : gameMode === mode.id}"
+                    class="w-full border-l border-t border-b text-base font-medium rounded-l-md text-black bg-white hover:bg-blue-100 px-4 py-2">
+              {{ mode.label }}
+            </button>
+          </div>
+          <v-select
+              v-model="civ"
+              :items="constantes.civ"
+              item-text="string"
+              item-value="id"
+              label="Select game mode"
+              single-line
+              solo
+              class="w-1/2"
+          ></v-select>
+
+          <CivStatsHeavyCard
+              :civ="constantes.civ.find(civ => civ.id === this.civ)" :game-mode="constantes.leaderboards.find(mode => mode.id === this.gameMode)" :player="player"></CivStatsHeavyCard>
+
+        </div>
+      </v-card-text>
 
 
-    <div class="flex flex-col">
-      <h1 class="text-3xl">{{player.name}}</h1>
-      <div class="flex items-between">
-
-        <GameMode v-if="player" :data="player" label="Global"/>
-        <GameMode v-for="mode in constantes.leaderboards" :key="mode.id" :data="player[mode.id]" :label="mode.label"/>
-      </div>
-      <div class="flex items-center my-10">
-        <button v-for="mode in constantes.leaderboards" :key="mode.id" @click="() => {gameMode = mode.id}" type="button"
-                :class="{'bg-blue-500' : gameMode === mode.id}"
-                class="w-full border-l border-t border-b text-base font-medium rounded-l-md text-black bg-white hover:bg-blue-100 px-4 py-2">
-          {{ mode.label }}
-        </button>
-      </div>
-      <v-select
-          v-model="civ"
-          :items="constantes.civ"
-          item-text="string"
-          item-value="id"
-          label="Select game mode"
-          single-line
-          solo
-          class="w-1/2"
-      ></v-select>
-
-      <CivStatsHeavyCard
-          :civ="constantes.civ.find(civ => civ.id === this.civ)" :game-mode="constantes.leaderboards.find(mode => mode.id === this.gameMode)" :player="player"></CivStatsHeavyCard>
-
-    </div>
-  </v-card>
 
 
-
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -117,7 +132,7 @@ export default {
     refreshData() {
       this.player = {...this.player}
     }
-  }
+  },
 }
 
 </script>
